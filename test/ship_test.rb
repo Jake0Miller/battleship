c022f90f94196811c83d25f514b46b81b0eaccf7
Oscar Bellechasse <obellech10@gmail.com>
2019-05-06 16:45:59 -0600

    Create test_that_it_exists method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,9 +4,12 @@ require './lib/ship'
 
 class ShipTest < MiniTest::Test
   def setup
-    
-
+    @cruiser = Ship.new("Cruiser", 3)
+    @sub = Ship.new("Sub", 2)
   end
 
+  def test_that_it_exists
+    assert_instance_of Ship, @cruiser 
+  end
 
 end
