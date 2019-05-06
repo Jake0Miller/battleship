4f2166776cdf94d9191a1f0d698250ba8b58ac66
Jake Miller <jake0miller@gmail.com>
2019-05-06 17:11:21 -0600

    Create test cell exists

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,10 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
+require './lib/cell'
 
 class CellTest < MiniTest::Test
   def setup
+    @cellB4 = Cell.new("B4")
+  end
 
-
+  def test_it_exists
+    assert_instance_of Cell, @cellB4
   end
 end
