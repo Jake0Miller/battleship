4220a2adb429eccccfcb45bb1432bd7db0a70a41
Oscar Bellechasse <obellech10@gmail.com>
2019-05-06 17:51:43 -0600

    Add hit method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,19 @@
 class Ship
   attr_reader :name, :length, :health
-  
+
   def initialize(name, length)
     @name = name
     @length = length
     @health = length
+    @sunk = false
+  end
+
+  def sunk?
+    @sunk
+  end
+
+  def hit
+    @health -= 1
   end
 
 end
