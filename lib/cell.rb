b245d12ee3f2e1e92300d0b8ac008d3333a64d67
Jake Miller <jake0miller@gmail.com>
2019-05-06 18:03:48 -0600

    Write place_ship and fired_upon

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,9 +4,18 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
     @ship == nil
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def place_ship(ship)
+    @ship = ship
   end
 end
