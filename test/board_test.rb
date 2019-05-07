a586117190a3e9d6d8ca85ba5e3e5e7aa7b7f6c5
Oscar Bellechasse <obellech10@gmail.com>
2019-05-07 15:26:11 -0600

    Add test_board_cells

--- test/board_test.rb
+++ test/board_test.rb
@@ -15,5 +15,13 @@ class BoardTest < MiniTest::Test
   def test_that_it_exists
     assert_instance_of Board, board
   end
+
+  def test_board_cells
+    assert_instance_of Hash, board.cells
+    assert_instance_of Cell, board.cells.first
+    assert_equal 16, board.cells.keys
+    assert_equal 16, board.cells.values
+  end
+
   
 end
