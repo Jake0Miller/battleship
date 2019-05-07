91ecd4357c6b3df3e2a5c9e7244b336a510087cf
Oscar Bellechasse <obellech10@gmail.com>
2019-05-07 15:30:15 -0600

    Add test_valid_coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,21 +7,29 @@ require 'pry'
 
 class BoardTest < MiniTest::Test
   def setup
-    board = Board.new
+    @board = Board.new
     # @cellB4 = Cell.new("B4")
     # @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_that_it_exists
-    assert_instance_of Board, board
+    assert_instance_of Board, @board
   end
 
   def test_board_cells
-    assert_instance_of Hash, board.cells
-    assert_instance_of Cell, board.cells.first
-    assert_equal 16, board.cells.keys
-    assert_equal 16, board.cells.values
+    assert_instance_of Hash, @board.cells
+    assert_instance_of Cell, @board.cells.first
+    assert_equal 16, @board.cells.keys
+    assert_equal 16, @board.cells.values
   end
 
+  def test_valid_coordinate?
+    assert @board.valid_coordinate?("A1")
+    assert @board.valid_coordinate?("D4")
+    refute @board.valid_coordinate?("A5")
+    refute @board.valid_coordinate?("E1")
+    refute @board.valid_coordinate?("A22")
+    assert @board.valid_coordinate?(:A2)
+  end
   
 end
