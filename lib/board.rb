a6921dcc37578289a03c071c23d3b96d2151ebab
Jake Miller <jake0miller@gmail.com>
2019-05-08 14:43:36 -0600

    Finish valid_placement method

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,18 @@ class Board
 
   def initialize
     @cells = {}
+    @board_size = 4
+    @board_letters = ""
+    @board_numbers = ""
     setup_board
   end
 
   def setup_board
-    ("A".."D").each do |letter|
-      (1..4).each do |num|
-        @cells["#{letter}#{num.to_s}"] = Cell.new("#{letter}#{num.to_s}")
+    (65.upto(64+@board_size)).each { |letter| @board_letters += letter.chr }
+    (1.upto(@board_size)).each { |num| @board_numbers += num.to_s }
+    @board_letters.split('').each do |letter|
+      @board_numbers.split('').each do |num|
+        @cells["#{letter.chr}#{num.to_s}"] = Cell.new("#{letter.chr}#{num.to_s}")
       end
     end
   end
@@ -17,15 +22,31 @@ class Board
   def valid_coordinate?(coord)
     return false if !coord.is_a? String
     return false if coord.length != 2
-    return false if !"ABCD".include?(coord[0])
-    return false if !"1234".include?(coord[1])
+    return false if !@board_letters.include?(coord[0])
+    return false if !@board_numbers.include?(coord[1])
     true
   end
 
   def valid_placement?(ship, coords)
     return false if ship.length != coords.length
     return false if coords.any? { |coord| valid_coordinate?(coord) == false }
-    
+    return check_letters_same(ship,coords) if coords[0][0] == coords[1][0]
+    return check_numbers_same(ship,coords) if coords[0][1] == coords[1][1]
+  end
+
+  def check_letters_same(ship,coords)
+    (ship.length-1).times do |i|
+      return false if coords[i][0] != coords[i+1][0]
+      return false if coords[i][1].next != coords[i+1][1]
+    end
+    true
+  end
+
+  def check_numbers_same(ship,coords)
+    (ship.length-1).times do |i|
+      return false if coords[i][0].next != coords[i+1][0]
+      return false if coords[i][1] != coords[i+1][1]
+    end
     true
   end
 end
