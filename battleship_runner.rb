aba09c7c73214d88c19dcfbaf903b007b27989a4
Jake Miller <jake0miller@gmail.com>
2019-05-09 15:22:15 -0600

    Write code to loop until all ships sunk

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -4,7 +4,6 @@ require './lib/board'
 require './lib/ship'
 require 'pry'
 
-
 def start
   puts "Welcome to BATTLESHIP"
   play?
@@ -26,6 +25,7 @@ def play?
   start_game
 end
 
+# THIS NEEDS TO BE UPDATED TO CHECK FOR BAD INPUT
 def create_boards
   puts "Select board size (4 to 26):"
   size = gets.chomp.to_i
@@ -36,6 +36,7 @@ def create_boards
   place_player_ships
 end
 
+# THIS NEEDS TO BE UPDATED
 def place_comp_ships
   cruiser = Ship.new("Cruiser", 3)
   sub = Ship.new("Submarine", 2)
@@ -44,35 +45,62 @@ def place_comp_ships
 end
 
 def place_player_ships
+  num_ships = set_num_ships
   puts "I have laid out my ships on the grid."
   puts "You now need to lay out your ships."
-  puts "The Cruiser is two units long and the Submarine is three units long."
+  ships = ask_for_ships(num_ships)
+  ships.each { |ship| place_ships(ship) }
+end
+
+def place_ships(ship)
+  cur_ship = Ship.new(ship[0], ship[1])
+  puts "The #{cur_ship.name} is #{cur_ship.length} units long."
   puts @player_board.render(true)
-  cruiser = Ship.new("Cruiser", 3)
-  sub = Ship.new("Submarine", 2)
 
-  def ask_for_coordinates(ship)
-    print "> "
-    coordinates = gets.chomp.split(" ")
-    if !@player_board.valid_placement?(ship, coordinates)
-      puts "Those are invalid coordinates. Please try again:"
-      ask_for_coordinates(ship)
-    end
-    @player_board.place(ship, coordinates)
+  puts "Enter the squares for the #{cur_ship.name} (#{cur_ship.length} spaces):"
+  example = "Example: "
+  cur_ship.length.times { |i| example << "A#{i+1} " }
+  puts example
+  ask_for_coordinates(cur_ship)
+end
+
+def ask_for_ships(num_ships)
+  ships = []
+  num_ships.times do
+    puts "How big should this ship be? (2-5)"
+    input_size = gets.chomp.to_i
+    puts "What do you want to call this ship? (eg Submarine)"
+    input_name = gets.chomp
+    ships << [input_name, input_size]
   end
+  return ships
+end
 
-  puts "Enter the squares for the Cruiser (3 spaces):"
-  puts "Example: A1 A2 A3"
-  ask_for_coordinates(cruiser)
-  puts "Enter the squares for the Sub (2 spaces):"
-  puts "Example: A1 A2"
-  ask_for_coordinates(sub)
+def set_num_ships
+  puts "How many ships do you want to play with? (1-5)"
+  input_number = gets.chomp.to_i
+  if input_number < 1 && input_number > 5
+    set_num_ships
+  else
+    return input_number
+  end
+end
+
+def ask_for_coordinates(ship)
+  print "> "
+  coordinates = gets.chomp.split(" ")
+  if !@player_board.valid_placement?(ship, coordinates)
+    puts "Those are invalid coordinates. Please try again:"
+    ask_for_coordinates(ship)
+  end
+  @player_board.place(ship, coordinates)
 end
 
 def start_game
-  take_turn
-  display_comp_board
-  display_player_board
+  until game_over?
+    take_turn
+  end
+  play?
 end
 
 def take_turn
@@ -91,11 +119,12 @@ def player_shot
   shot = gets.chomp
   if !@comp_board.valid_coordinate?(shot) || @comp_board.cells[shot].fired_upon?
     puts "Please enter a valid coordinate (eg B2):"
-    player_shot
+    shot = player_shot
   end
   shot
 end
 
+# THIS NEEDS TO BE UPDATED. A LOT.
 def comp_shot
   "A3"
 end
@@ -130,6 +159,18 @@ end
 def display_player_board
   puts "==============PLAYER BOARD=============="
   puts @player_board.render(true)
+end
+
+def game_over?
+  if @player_board.all_ships_sunk
+    puts "I won!"
+    return true
+  end
+  if @comp_board.all_ships_sunk
+    puts "You won!"
+    return true
+  end
+  false
 end
 
 start
